6e0f51dbc17c719b3219bd8492e85a47e3428b85
vagrant <vagrant@homestead>
2020-09-01 06:06:54 +0000

    fix the bug for authorization

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -6,28 +6,31 @@ use App\Http\Requests\Api\AuthorizationRequest;
 use App\Models\User;
 use Illuminate\Auth\AuthenticationException;
 use Overtrue\LaravelSocialite\Socialite;
+use PhpParser\Node\Expr\Throw_;
 
 class AuthorizationsController extends Controller
 {
     public function store(AuthorizationRequest $request)
     {
-       $driver = Socialite::driver('wechat');
+        $driver = Socialite::driver('wechat');
 
-       $accessToken = $driver->getAccessToken($request->code);
+        try {
+            $accessToken = $driver->getAccessToken($request->code);
 
-       if (!$accessToken) {
-           throw new AuthenticationException('code不正确!');
-       }
+            $oauthUser = $driver->user($accessToken);
 
-       $oauthUser = $driver->user($accessToken);
+        } catch (\Exception $e) {
+            throw new AuthenticationException('参数错误，未获取用户信息！');
+        }
 
-       if (!$user = User::where('open_id', $oauthUser->getId())->first()) {
-           $user = User::create([
-               'name' => $oauthUser->getNickname(),
-               'avatar' => $oauthUser->getAvatar(),
-               'open_id' => $oauthUser->getId(),
-           ]);
-       }
+
+        if (!$user = User::where('open_id', $oauthUser->getId())->first()) {
+            $user = User::create([
+                'name' => $oauthUser->getNickname(),
+                'avatar' => $oauthUser->getAvatar(),
+                'open_id' => $oauthUser->getId(),
+            ]);
+        }
 
         $token = auth('api')->login($user);
 
