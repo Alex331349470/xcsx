b1389bd45f7c92e863a4774a4b7dfa2ced8c0b11
vagrant <vagrant@homestead>
2020-09-12 08:54:39 +0000

    test the order pay

--- app/Http/Controllers/Api/PaymentsController.php
+++ app/Http/Controllers/Api/PaymentsController.php
@@ -9,14 +9,20 @@ class PaymentsController extends Controller
 {
     public function payByWechat(Order $order)
     {
-        if ($order->paid_at || $order->left_time > 0) {
-            abort(403,'订单不正确');
-        }
+//        if ($order->paid_at || $order->left_time > 0) {
+//            abort(403,'订单不正确');
+//        }
+
+//        $wechatOrder = app('wechat_pay')->scan([
+//            'out_trade_no' => $order->no,
+//            'total_fee' => $order->price * 100,
+//            'body' => '支付订单：'. $order->no,
+//        ]);
 
         $wechatOrder = app('wechat_pay')->scan([
-            'out_trade_no' => $order->no,
-            'total_fee' => $order->price * 100,
-            'body' => '支付订单：'. $order->no,
+            'out_trade_no' => time(),
+            'total_fee' => 1,
+            'body' => '支付订单:' . time()
         ]);
 
         $qrCode = new QrCode($wechatOrder->code_url);
