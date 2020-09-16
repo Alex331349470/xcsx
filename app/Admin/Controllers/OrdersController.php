6b161354beef036485433957725a05e590bd5426
vagrant <vagrant@homestead>
2020-09-16 19:55:09 +0000

    order status fixed

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -27,6 +27,8 @@ class OrdersController extends AdminController
     protected function grid()
     {
         $grid = new Grid(new Order());
+
+        $grid->model()->whereNotNull('paid_at')->orderBy('paid_at','desc');
         $grid->column('id', __('Id值'));
         $grid->column('car', __('车辆名称'))->display(function () {
             $name = Car::query()->where('id', $this->car_id)->first()->name;
