8bab13403a19e3aabf7ddf6ad02677e3379a2dfd
vagrant <vagrant@homestead>
2020-09-23 14:03:42 +0000

    add

--- resources/views/order/order.blade.php
+++ resources/views/order/order.blade.php
@@ -1,11 +1,11 @@
-<canvas id="myChart" width="400" height="400"></canvas>
+<canvas id="myChart" width="200" height="200"></canvas>
 <script>
     $(function () {
         var ctx = document.getElementById("myChart").getContext('2d');
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                labels: ["red", "Blue", "Yellow", "Green", "Purple", "Orange"],
                 datasets: [{
                     label: '# of Votes',
                     data: [12, 19, 3, 5, 2, 3],
