cac7747a62b932ef33c36191264f5fa85af41210
vagrant <vagrant@homestead>
2020-09-23 10:56:40 +0000

    test

--- app/Admin/Controllers/SellItemsController.php
+++ app/Admin/Controllers/SellItemsController.php
@@ -35,10 +35,10 @@ class SellItemsController extends AdminController
         $grid->column('id', __('支付码-ID'))->qrcode(function ($value)  {
             $car = SellItem::query()->where('id', $value)->first();
 
-            if (!$car->car_id) {
-                $response = new Response();
-                return $response->error('请选择车辆')->refresh();
-            }
+//            if (!$car) {
+//                $response = new Response();
+//                return $response->error('请选择车辆')->refresh();
+//            }
 
             $url = 'http://car.agelove.cn/api/v1/cars/' . $car->car_id . '/sell_items/' . $value . '/payment ';
             $ch = curl_init();
