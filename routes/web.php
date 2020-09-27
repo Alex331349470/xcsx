efc5608b7b4a44cb6c8d1410495007fb794f52cf
vagrant <vagrant@homestead>
2020-09-27 07:13:06 +0000

    get openid

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,6 @@ Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
     Route::get('/user', function () {
         $user = session('wechat.oauth_user.default'); // 拿到授权用户资料
 
-        dd($user);
+        return $user['id'];
     });
 });
