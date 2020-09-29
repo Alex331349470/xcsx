4f08bb01c4feb7f103e8a6e137265b00ed64c436
vagrant <vagrant@homestead>
2020-09-29 09:50:32 +0000

    test

--- app/Admin/Actions/Post/Stop.php
+++ app/Admin/Actions/Post/Stop.php
@@ -3,6 +3,8 @@
 namespace App\Admin\Actions\Post;
 
 use App\Models\Car;
+use App\Models\User;
+use Carbon\Carbon;
 use Encore\Admin\Actions\RowAction;
 use GuzzleHttp\Client;
 use Illuminate\Database\Eloquent\Model;
@@ -47,6 +49,28 @@ class Stop extends RowAction
         $car->update([
             'status' => 0,
         ]);
+        $officialAccount = \EasyWeChat::officialAccount();
+
+        $users = User::all();
+
+        foreach ($users as $user) {
+
+            if ($openId = $user->openId) {
+                $sub_data = [
+                    'touser' => $openId,
+//                    'touser' => 'otSh7szfR7tBPNcNzk45CgZUgdW4',
+                    'template_id' => '28JqHbTcIMEHHS7JMkYyLp-zUQhWorLv1SADPcPVXJg',
+                    'data' => [
+                        'first' => '车辆故障暂停',
+                        'event' => '由于'.$car->name.'训练车故障，暂时暂停',
+                        'finish_time' => Carbon::now()->toDateTimeString(),
+                        'remark' => '车辆故障，暂停计时，请修复该车或变更该订单训练车辆',
+                    ],
+                ];
+
+                $officialAccount->template_message->send($sub_data);
+            }
+        }
 
         return $this->response()->success('停止成功')->refresh();
     }
