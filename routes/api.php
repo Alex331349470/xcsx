9d6e8eda1f45a65b9a81eb3b058eaa5f992284ce
vagrant <vagrant@homestead>
2020-10-10 19:21:42 +0000

    test

--- routes/api.php
+++ routes/api.php
@@ -41,93 +41,52 @@ Route::prefix('v1')
                 //测试
                 Route::get('test', 'TestsController@pay')
                     ->name('test');
+
                 //非Authorization的api
-                //用户信息
-                Route::get('user', 'UsersController@me')
-                    ->name('user.show');
-                //套餐列表
-                Route::get('sell_items', 'SellItemsController@index')
-                    ->name('sell_items.index');
-                //套餐详情
-                Route::get('sell_items/{sell_item}','SellItemsController@show')
-                    ->name('sell_items.show');
-                //套餐修改
-                Route::put('sell_items/{sell_item}','SellItemsController@update')
-                    ->name('sell_items.update');
-                //套餐删除
-                Route::delete('sell_items/{sell_item}','SellItemsController@destroy')
-                    ->name('sell_items.destroy');
-                //车辆列表
-                Route::get('cars', 'CarsController@index')
-                    ->name('cars.index');
-                //车辆详情
-                Route::get('cars/{car}', 'CarsController@show')
-                    ->name('cars.show');
-                //车辆修改
-                Route::put('cars/{car}','CarsController@update')
-                    ->name('cars.update');
-                //车辆删除
-                Route::delete('cars/{car}','CarsController@destroy')
-                    ->name('cars.destroy');
-                //车辆控制
-                Route::post('car/control', 'CarsController@controlCar')
-                    ->name('car.control');
-                //订单列表
-                Route::get('orders', 'OrdersController@index')
-                    ->name('orders.index');
-                //订单详情
-                Route::get('orders/{order}', 'OrdersController@show')
-                    ->name('orders.show');
-                //订单停止
-                Route::post('orders/{order}/stop', 'OrdersController@stop')
-                    ->name('order.stop');
-                //订单续签
-                Route::post('orders/{order}/cars/{car}/start', 'OrdersController@start')
-                    ->name('order.start');
-//                Route::middleware('auth:api')->group(function () {
-//                    //用户信息
-//                    Route::get('user', 'UsersController@me')
-//                        ->name('user.show');
-//                    //套餐列表
-//                    Route::get('sell_items', 'SellItemsController@index')
-//                        ->name('sell_items.index');
-//                    //套餐详情
-//                    Route::get('sell_items/{sell_item}','SellItemsController@show')
-//                        ->name('sell_items.show');
-//                    //套餐修改
-//                    Route::put('sell_items/{sell_item}','SellItemsController@update')
-//                        ->name('sell_items.update');
-//                    //套餐删除
-//                    Route::delete('sell_items/{sell_item}','SellItemsController@destroy')
-//                        ->name('sell_items.destroy');
-//                    //车辆列表
-//                    Route::get('cars', 'CarsController@index')
-//                        ->name('cars.index');
-//                    //车辆详情
-//                    Route::get('cars/{car}', 'CarsController@show')
-//                        ->name('cars.show');
-//                    //车辆修改
-//                    Route::put('cars/{car}','CarsController@update')
-//                        ->name('cars.update');
-//                    //车辆删除
-//                    Route::delete('cars/{car}','CarsController@destroy')
-//                        ->name('cars.destroy');
-//                    //车辆控制
-//                    Route::post('car/control', 'CarsController@controlCar')
-//                        ->name('car.control');
-//                    //订单列表
-//                    Route::get('orders', 'OrdersController@index')
-//                        ->name('orders.index');
-//                    //订单详情
-//                    Route::get('orders/{order}', 'OrdersController@show')
-//                        ->name('orders.show');
-//                    //订单停止
-//                    Route::post('orders/{order}/stop', 'OrdersController@stop')
-//                        ->name('order.stop');
-//                    //订单续签
-//                    Route::post('orders/{order}/cars/{car}/start', 'OrdersController@start')
-//                        ->name('order.start');
-//
-//                });
+                Route::middleware('auth:api')->group(function () {
+                    //用户信息
+                    Route::get('user', 'UsersController@me')
+                        ->name('user.show');
+                    //套餐列表
+                    Route::get('sell_items', 'SellItemsController@index')
+                        ->name('sell_items.index');
+                    //套餐详情
+                    Route::get('sell_items/{sell_item}','SellItemsController@show')
+                        ->name('sell_items.show');
+                    //套餐修改
+                    Route::put('sell_items/{sell_item}','SellItemsController@update')
+                        ->name('sell_items.update');
+                    //套餐删除
+                    Route::delete('sell_items/{sell_item}','SellItemsController@destroy')
+                        ->name('sell_items.destroy');
+                    //车辆列表
+                    Route::get('cars', 'CarsController@index')
+                        ->name('cars.index');
+                    //车辆详情
+                    Route::get('cars/{car}', 'CarsController@show')
+                        ->name('cars.show');
+                    //车辆修改
+                    Route::put('cars/{car}','CarsController@update')
+                        ->name('cars.update');
+                    //车辆删除
+                    Route::delete('cars/{car}','CarsController@destroy')
+                        ->name('cars.destroy');
+                    //车辆控制
+                    Route::post('car/control', 'CarsController@controlCar')
+                        ->name('car.control');
+                    //订单列表
+                    Route::get('orders', 'OrdersController@index')
+                        ->name('orders.index');
+                    //订单详情
+                    Route::get('orders/{order}', 'OrdersController@show')
+                        ->name('orders.show');
+                    //订单停止
+                    Route::post('orders/{order}/stop', 'OrdersController@stop')
+                        ->name('order.stop');
+                    //订单续签
+                    Route::post('orders/{order}/cars/{car}/start', 'OrdersController@start')
+                        ->name('order.start');
+
+                });
             });
     });
