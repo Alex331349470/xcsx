0e495d8cf0340c259018a6ac888357967b5cff78
vagrant <vagrant@homestead>
2020-10-10 23:24:08 +0000

    pay

--- app/Admin/Actions/Post/Pay.php
+++ app/Admin/Actions/Post/Pay.php
@@ -15,7 +15,7 @@ class Pay extends RowAction
         // $model ...
 
         $openid = (new \Encore\Admin\Admin)->user()->openId;
-        $url = env('APP_URL').'/api/v1/test';
+        $url = env('APP_URL') . '/api/v1/test';
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
@@ -29,21 +29,21 @@ class Pay extends RowAction
         curl_close($ch);
 
 
-//        Admin::script('WeixinJSBridge.invoke(
-//                    \'getBrandWCPayRequest\', {
-//                        "appId": '.$data->appId.',     //公众号名称，由商户传入
-//                        "timeStamp": '.$data->timeStamp.',         //时间戳，自1970年以来的秒数
-//                        "nonceStr": '.$data->nonceStr.', //随机串
-//                        "package":'. $data->package.',
-//                        "signType":'. $data->signType.',         //微信签名方式：
-//                        "paySign":'. $data->paySign.' //微信签名
-//                    },
-//                    function (res) {
-//                        if (res.err_msg == "get_brand_wcpay_request:ok") {
-//                            // 使用以上方式判断前端返回,微信团队郑重提示：
-//                            //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-//                        }
-//                    });');
+        Admin::script('WeixinJSBridge.invoke(
+                    \'getBrandWCPayRequest\', {
+                        "appId": ' . $data->appId . ',     //公众号名称，由商户传入
+                        "timeStamp": ' . $data->timeStamp . ',         //时间戳，自1970年以来的秒数
+                        "nonceStr": ' . $data->nonceStr . ', //随机串
+                        "package":' . $data->package . ',
+                        "signType":' . $data->signType . ',         //微信签名方式：
+                        "paySign":' . $data->paySign . ' //微信签名
+                    },
+                    function (res) {
+                        if (res.err_msg == "get_brand_wcpay_request:ok") {
+                            // 使用以上方式判断前端返回,微信团队郑重提示：
+                            //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
+                        }
+                    });');
         return $this->response()->success($data)->refresh();
     }
 
