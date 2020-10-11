1296cc8089117c5565e30129d0f2286af6138f91
vagrant <vagrant@homestead>
2020-10-11 02:01:31 +0000

    test

--- app/Admin/Actions/Post/Pay.php
+++ app/Admin/Actions/Post/Pay.php
@@ -12,6 +12,9 @@ class Pay extends RowAction
 
     public function handle(Model $model)
     {
+        // $model ...
+
+        $openid = (new \Encore\Admin\Admin)->user()->openId;
         $url = env('APP_URL') . '/api/v1/test';
         $ch = curl_init();
 
@@ -21,11 +24,26 @@ class Pay extends RowAction
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
 
-        $data = curl_exec($ch);
+        $data = json_decode(curl_exec($ch), true);
         curl_close($ch);
 
-
-        return $this->response()->info($data);
+//        Admin::script('WeixinJSBridge.invoke(
+//                    \'getBrandWCPayRequest\', {
+//                        "appId": ' . $data['appId'] . ',     //公众号名称，由商户传入
+//                        "timeStamp": ' . $data['timeStamp'] . ',         //时间戳，自1970年以来的秒数
+//                        "nonceStr": ' . $data['nonceStr'] . ', //随机串
+//                        "package":' . $data['package'] . ',
+//                        "signType":' . $data['signType'] . ',         //微信签名方式：
+//                        "paySign":' . $data['paySign'] . ' //微信签名
+//                    },
+//                    function (res) {
+//                        if (res.err_msg == "get_brand_wcpay_request:ok") {
+//                            console.log(res.err_msg)
+//                            // 使用以上方式判断前端返回,微信团队郑重提示：
+//                            //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
+//                        }
+//                    });');
+        return $this->response()->info('支付');
     }
 
 }
