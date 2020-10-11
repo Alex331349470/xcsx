2adbd9ab793ac430388b69be015535de52703f27
vagrant <vagrant@homestead>
2020-10-11 02:02:17 +0000

    test

--- app/Http/Controllers/Api/TestsController.php
+++ app/Http/Controllers/Api/TestsController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use EasyWeChat\Factory;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
@@ -30,11 +29,11 @@ class TestsController extends Controller
         $officialAccount->template_message->send($sub_data);
 
         $serial_num = '32094';
-        $ws = new \WebSocket\Client('wss://mobi.ydsyb123.com:8282/?dev_id=' . $serial_num . '&member_id=319');
+        $ws = new \WebSocket\Client('wss://mobi.ydsyb123.com:8282/?dev_id='.$serial_num.'&member_id=319');
 
         $client = new Client();
 
-        $client->get('https://mobi.ydsyb123.com/api/send2sb.php', [
+        $client->get('https://mobi.ydsyb123.com/api/send2sb.php',[
             'query' => [
                 'us_id' => env('CAR_US_ID'),
                 'openid' => env('CAR_OPEN_ID'),
@@ -43,10 +42,10 @@ class TestsController extends Controller
             ]
         ]);
 
-        $msg = json_decode($ws->receive(), true);
+        $msg = json_decode($ws->receive(),true);
         $ws->close();
 
-        $time = substr($msg['msg'], 4, 4);
+        $time = substr($msg['msg'],4,4);
 
         $change = hexdec($time);
 
@@ -55,24 +54,13 @@ class TestsController extends Controller
 
     public function pay(Request $request)
     {
-        $config = [
-            'app_id' => config('pay.wechat.app_id'),
-            'mch_id' => config('pay.wechat.mch_id'),
-            'key' => config('pay.wechat.key'),
-            'notify_url' => env('APP_URL') . '/payment/wechat/notify'
-        ];
-
-        $app = Factory::payment($config);
         $order = [
             'out_trade_no' => time(),
             'body' => 'subject-test',
             'total_fee' => '1',
-            'trade_type' => 'JSAPI',
             'openid' => 'otSh7szfR7tBPNcNzk45CgZUgdW4'
         ];
 
-        return $pay = $app->order->unify($order);
-
-//       return $pay = app('wechat_pay')->mp($order);
+       return $pay = app('wechat_pay')->mp($order);
     }
 }
