f2f47dcac83947d017493f9ec23d18cb7841c6d6
vagrant <vagrant@homestead>
2020-10-12 08:15:46 +0000

    test

--- app/Http/Controllers/Api/PaymentsController.php
+++ app/Http/Controllers/Api/PaymentsController.php
@@ -44,7 +44,6 @@ class PaymentsController extends Controller
         $school_name = DriverSchool::query()->where('id', $car->driver_school_id)->first()->name;
         $school_pinyin = pinyin_abbr($school_name);
 
-        dd($school_name);
 
         $order = Order::create([
             'car_id' => $car->id,
