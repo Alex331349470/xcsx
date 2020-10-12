115ee6ce993975943f24c5841cbcd9a2c8a48c6e
vagrant <vagrant@homestead>
2020-10-12 06:10:57 +0000

    tst

--- app/Http/Controllers/Api/PaymentsController.php
+++ app/Http/Controllers/Api/PaymentsController.php
@@ -41,6 +41,7 @@ class PaymentsController extends Controller
 
     public function paySoon(Car $car, SellItem $sellItem, Request $request)
     {
+        dd($car);
         $school_name = DriverSchool::query()->where('id', $car->driver_school_id)->first()->name;
         $school_pinyin = pinyin_abbr($school_name);
 
