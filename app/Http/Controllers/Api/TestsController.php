a707e0b4a472bb1d6b68c07117b9bcf52c173072
vagrant <vagrant@homestead>
2020-10-14 04:14:28 +0000

    tst

--- app/Http/Controllers/Api/TestsController.php
+++ app/Http/Controllers/Api/TestsController.php
@@ -28,7 +28,7 @@ class TestsController extends Controller
 //
 //        $officialAccount->template_message->send($sub_data);
 
-        $serial_num = '32074';
+        $serial_num = '32103';
         $ws = new \WebSocket\Client('wss://mobi.ydsyb123.com:8282/?dev_id='.$serial_num.'&member_id=319');
 
         $client = new Client();
