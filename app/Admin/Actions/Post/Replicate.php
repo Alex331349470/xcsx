c79f9358b4eb53cc45a0d321f071717495e4de09
vagrant <vagrant@homestead>
2020-10-17 03:29:55 +0000

    test

--- app/Admin/Actions/Post/Replicate.php
+++ app/Admin/Actions/Post/Replicate.php
@@ -12,30 +12,31 @@ class Replicate extends RowAction
 
     public function handle(Model $model)
     {
-        $serial_num = $model->serial_num;
+        try {
+            $serial_num = $model->serial_num;
 
-        $ws = new \WebSocket\Client('wss://mobi.ydsyb123.com:8282/?dev_id=' . $serial_num . '&member_id=319');
+            $ws = new \WebSocket\Client('wss://mobi.ydsyb123.com:8282/?dev_id=' . $serial_num . '&member_id=319');
 
-        $client = new Client();
+            $client = new Client();
 
-        $client->get('https://mobi.ydsyb123.com/api/send2sb.php', [
-            'query' => [
-                'us_id' => env('CAR_US_ID'),
-                'openid' => env('CAR_OPEN_ID'),
-                'dev_id' => $serial_num,
-                'msg' => 'd100'
-            ]
-        ]);
-        $message = $ws->receive();
+            $client->get('https://mobi.ydsyb123.com/api/send2sb.php', [
+                'query' => [
+                    'us_id' => env('CAR_US_ID'),
+                    'openid' => env('CAR_OPEN_ID'),
+                    'dev_id' => $serial_num,
+                    'msg' => 'd100'
+                ]
+            ]);
+            $message = $ws->receive();
 
-        $ws->close();
+            $ws->close();
 
-        $check = json_decode($message,true);
+            $msg = json_decode($message, true);
 
-        if ($check['msg']) {
-            return $this->response()->success('设备在线')->refresh();
-
-        } else {
+            if ($msg['msg']) {
+                return $this->response()->success('设备在线')->refresh();
+            }
+        } catch (\Exception $exception) {
             return $this->response()->error('设备未在线')->refresh();
         }
     }
