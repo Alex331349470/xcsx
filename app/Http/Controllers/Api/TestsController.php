a82e4d9d1e4b8e45d71b492db28d427f8b8f6575
vagrant <vagrant@homestead>
2020-10-23 02:59:11 +0000

    test

--- app/Http/Controllers/Api/TestsController.php
+++ app/Http/Controllers/Api/TestsController.php
@@ -53,16 +53,16 @@ class TestsController extends Controller
 
             $ws = new \WebSocket\Client('wss://mobi.ydsyb123.com:8282/?dev_id=' . $serial_num . '&member_id=319');
 
-//            $client = new Client();
-//
-//            $client->get('https://mobi.ydsyb123.com/api/send2sb.php', [
-//                'query' => [
-//                    'us_id' => env('CAR_US_ID'),
-//                    'openid' => env('CAR_OPEN_ID'),
-//                    'dev_id' => $serial_num,
-//                    'msg' => 'd100'
-//                ]
-//            ]);
+            $client = new Client();
+
+            $client->get('https://mobi.ydsyb123.com/api/send2sb.php', [
+                'query' => [
+                    'us_id' => env('CAR_US_ID'),
+                    'openid' => env('CAR_OPEN_ID'),
+                    'dev_id' => $serial_num,
+                    'msg' => 'd100'
+                ]
+            ]);
 //            $message = $ws->receive();
 
             $ws->close();
