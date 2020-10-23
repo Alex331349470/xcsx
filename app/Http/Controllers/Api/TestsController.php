6246b0df074c5bcd4cd743993826cd92259c4909
vagrant <vagrant@homestead>
2020-10-23 02:55:06 +0000

    test

--- app/Http/Controllers/Api/TestsController.php
+++ app/Http/Controllers/Api/TestsController.php
@@ -28,24 +28,53 @@ class TestsController extends Controller
 //
 //        $officialAccount->template_message->send($sub_data);
 
-        $serial_num = '32103';
-        $ws = new \WebSocket\Client('wss://mobi.ydsyb123.com:8282/?dev_id='.$serial_num.'&member_id=319');
+        $serial_num = '32148';
 
-        $client = new Client();
+//        $ws = new \WebSocket\Client('wss://mobi.ydsyb123.com:8282/?dev_id='.$serial_num.'&member_id=319');
+//
+//        $client = new Client();
+//
+//        $client->get('https://mobi.ydsyb123.com/api/send2sb.php',[
+//            'query' => [
+//                'us_id' => env('CAR_US_ID'),
+//                'openid' => env('CAR_OPEN_ID'),
+//                'dev_id' => $serial_num,
+//                'msg' => 'd100'
+//            ]
+//        ]);
+//
+//        $msg = json_decode($ws->receive(),true);
+//        $ws->close();
+//
+//        return $msg;
 
-        $client->get('https://mobi.ydsyb123.com/api/send2sb.php',[
-            'query' => [
-                'us_id' => env('CAR_US_ID'),
-                'openid' => env('CAR_OPEN_ID'),
-                'dev_id' => $serial_num,
-                'msg' => 'd100'
-            ]
-        ]);
+        try {
+            $serial_num = $serial_num;
 
-        $msg = json_decode($ws->receive(),true);
-        $ws->close();
+            $ws = new \WebSocket\Client('wss://mobi.ydsyb123.com:8282/?dev_id=' . $serial_num . '&member_id=319');
 
-        return $msg;
+//            $client = new Client();
+//
+//            $client->get('https://mobi.ydsyb123.com/api/send2sb.php', [
+//                'query' => [
+//                    'us_id' => env('CAR_US_ID'),
+//                    'openid' => env('CAR_OPEN_ID'),
+//                    'dev_id' => $serial_num,
+//                    'msg' => 'd100'
+//                ]
+//            ]);
+//            $message = $ws->receive();
+
+            $ws->close();
+
+//            $msg = json_decode($message, true);
+//
+//            if ($msg['msg']) {
+//                return $this->response()->success('设备在线')->refresh();
+//            }
+        } catch (\Exception $exception) {
+            return $this->response()->error('设备未在线')->refresh();
+        }
 
 
 //        $time = substr($msg['msg'],4,4);
